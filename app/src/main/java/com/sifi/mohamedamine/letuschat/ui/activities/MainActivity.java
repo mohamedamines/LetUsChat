57c751a8af670ddbc5d1cff6db56cd1306e13b75
Mohamed Amine Sifi <masifi@anypli.com>
2016-11-28 10:09:01 +0100

    stored photo in Firebase storage

--- app/src/main/java/com/sifi/mohamedamine/letuschat/ui/activities/MainActivity.java
+++ app/src/main/java/com/sifi/mohamedamine/letuschat/ui/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sifi.mohamedamine.letuschat.ui.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -26,6 +28,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.sifi.mohamedamine.letuschat.R;
 import com.sifi.mohamedamine.letuschat.adapters.MessageAdapter;
 import com.sifi.mohamedamine.letuschat.entities.FriendlyMessage;
@@ -56,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener mChildEventListener;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference mChatPhotosStorageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,8 +72,10 @@ public class MainActivity extends AppCompatActivity {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseStorage = FirebaseStorage.getInstance();
 
         mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        mChatPhotosStorageReference = mFirebaseStorage.getReference().child("chat_photos");
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -156,6 +165,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == RC_SIGN_IN) {
 
             if (resultCode == RESULT_OK) {
@@ -165,7 +175,28 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Sign in canceled!", Toast.LENGTH_SHORT).show();
                 finish();
             }
-        }
+
+        } else if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+                Uri selectedImageUri = data.getData();
+
+                // Get a reference to store file at chat_photos/<FILENAME>
+                StorageReference photoRef  = mChatPhotosStorageReference.child(selectedImageUri.getLastPathSegment());
+
+                // Upload file to Firebase Storage
+                photoRef.putFile(selectedImageUri).addOnSuccessListener(this,
+                        new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        FriendlyMessage friendlyMessage = new FriendlyMessage(null, mUsername,
+                                downloadUrl.toString());
+                        mMessagesDatabaseReference.push().setValue(friendlyMessage);
+                    }
+                });
+
+            }
+
     }
 
     @Override
